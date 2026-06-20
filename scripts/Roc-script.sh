f316fdc6d85976f574aa035a5d4540844c3fec18
ytuyiuy454878989 <danfwu1@qq.com>
2026-06-21 07:31:29 +0800

    Update Roc-script.sh

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -85,10 +85,10 @@ git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
 #自行添加的app
 #git clone --depth=1 https://github.com/kenzok8/small-package/tree/main/luci-app-adguardhome package/luci-app-adguardhome
-git clone --depth=1 https://github.com/kenzok8/small-package/tree/main/luci-app-quickfile package/luci-app-quickfile
-git clone --depth=1 https://github.com/kenzok8/small-package/tree/main/luci-app-xunlei package/luci-app-xunlei
-git clone --depth=1 https://github.com/kenzok8/small-package/tree/main/luci-app-easytier package/luci-app-easytier
-git clone --depth=1 https://github.com/gngpp/thunder package/thunder
+#git clone --depth=1 https://github.com/kenzok8/small-package/tree/main/luci-app-quickfile package/luci-app-quickfile
+#git clone --depth=1 https://github.com/kenzok8/small-package/tree/main/luci-app-xunlei package/luci-app-xunlei
+#git clone --depth=1 https://github.com/kenzok8/small-package/tree/main/luci-app-easytier package/luci-app-easytier
+#git clone --depth=1 https://github.com/gngpp/thunder package/thunder
 
 ### PassWall & OpenClash ###
 
