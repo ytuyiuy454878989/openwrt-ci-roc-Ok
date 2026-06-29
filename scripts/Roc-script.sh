fdb6afe02855311eabfbf59cc29f50519dae4883
ytuyiuy454878989 <danfwu1@qq.com>
2026-06-29 13:27:27 +0800

    Update Roc-script.sh

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -84,11 +84,11 @@ git clone --depth=1 https://github.com/laipeng668/luci-app-gecoosac package/luci
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
 #自行添加的app
-#git clone --depth=1 https://github.com/laipeng668/luci/tree/master/applications/luci-app-adguardhome
-#git clone --depth=1 https://github.com/kenzok8/small-package/tree/main/luci-app-quickfile package/luci-app-quickfile
-#git clone --depth=1 https://github.com/kenzok8/small-package/tree/main/luci-app-xunlei package/luci-app-xunlei
-#git clone --depth=1 https://github.com/kenzok8/small-package/tree/main/luci-app-easytier package/luci-app-easytier
-#git clone --depth=1 https://github.com/gngpp/thunder package/thunder
+git clone https://github.com/kenzok78/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/sbwml/luci-app-quickfile package/quickfile
+git clone https://github.com/sbwml/luci-app-xunlei package/xunlei
+git clone https://github.com/EasyTier/luci-app-easytier.git package/luci-app-easytier
+git clone https://github.com/gngpp/thunder package/thunder
 #自行添加的app
 #git clone --depth=1 https://github.com/kenzok8/small-package/tree/main/luci-app-adguardhome package/luci-app-adguardhome
 #git clone --depth=1 https://github.com/kenzok8/small-package/tree/main/luci-app-quickfile package/luci-app-quickfile
